4dd9f4be6f1565c6c6c16a68509319564c43cda2
kellyeryan <kellyeryan@gmail.com>
2019-09-13 16:06:07 -0400

    Finish student scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,78 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-  end
+  attr_accessor :name, :location, :profile_url, :twitter, :linkedin, :github, :blog, :profile_quote, :bio
+
 
   def self.scrape_profile_page(profile_url)
-    
+
+    profile_page = Nokogiri::HTML(open(profile_url))
+
+
+
+    student = {}
+
+    links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+
+    # social_profile = profile_page.css("div.vitals-container").css("a")
+
+    # array_1 = social_profile.map do |link|
+    #   link.attribute("href").value
+    # end
+    #         array_1.each do |link|
+    #           link.include?("github")
+    #             student[:github] = link
+    #           link.include?("twitter")
+    #             student[:twitter] = link
+    #           link.include?("linkedin")
+    #             student[:linkedin] = link
+
+    student[:profile_quote] = profile_page.css("div.profile-quote").text unless !:profile_quote
+    student[:bio] = profile_page.css("div.description-holder").css("p").text unless !:bio
+
+    student
+  end
+
+
+  def self.scrape_index_page(index_url)
+
+    index_page = Nokogiri::HTML(open(index_url))
+
+    students = []
+    # index_page.css("div.roster-cards-container").each do |student|
+
+    # student_name = student.css("h4").first.text
+    # student_location = student.css("p").first.text
+    # student_profile_url = student.css("a")[0].first[1]
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+
+        student_profile_link = "#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+
+    students <<
+    {
+      :name  => student_name, :location  => student_location, :profile_url  => student_profile_link
+    }
+      end
+    end
+    students
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,38 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = name
+    @location = location
+
+    student_hash.each do |key, value|
+      if key.to_s.include?("name")
+        self.name = value
+      else self.location = value
+      end
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
+
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |att, value|
+      self.send("#{att}=", value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
